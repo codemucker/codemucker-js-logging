3277b87196e3db915f2dfb573a77e975c884dd59
Bert van Brakel <bertvanbrakel@gmail.com>
2020-10-29 15:59:18 +1000

    format tests

--- test/test.ts
+++ test/test.ts
@@ -1,25 +1,24 @@
 import { getLogger } from '@/index'
 
-describe("smoke tests", () =>{
-	const log = getLogger('my-test-logger')
+describe('smoke tests', () => {
+  const log = getLogger('my-test-logger')
 
-	test("can create logger", () => {
-	  expect(log).not.toBeFalsy()
-	})
-	test("can log trace", () => {
-	  log.trace("some trace log entry")
-	})
-	test("can log debug", () => {
-	  log.debug("some debug log entry")
-	})
-	test("can log info", () => {
-	  log.info("some info log entry")
-	})
-	test("can log warn", () => {
-	  log.warn("some warn log entry (ignore)")
-	})
-	test("can log error", () => {
-	  log.error("some error log entry (ignore)")
-	})
-
+  test('can create logger', () => {
+    expect(log).not.toBeFalsy()
+  })
+  test('can log trace', () => {
+    log.trace('some trace log entry')
+  })
+  test('can log debug', () => {
+    log.debug('some debug log entry')
+  })
+  test('can log info', () => {
+    log.info('some info log entry')
+  })
+  test('can log warn', () => {
+    log.warn('some warn log entry (ignore)')
+  })
+  test('can log error', () => {
+    log.error('some error log entry (ignore)')
+  })
 })
